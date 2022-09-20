cda1c7fb02bbc37303b4c75034bdf3f759a4eceb
Chris Lee <cl114@illinois.edu>
2022-09-20 17:35:29 -0500

    version 1

--- spoofer.js
+++ spoofer.js
@@ -2,7 +2,7 @@
  * Spiteful Spoofer
  * @version 1.0
  * @author Chris Lee <cl114@illinois.edu>
- * code was original written for another project : Check it out below!
+ * code was originally written for another project : Check it out below!
  * Project Helper Function : https://github.com/cleeclee123/Project-Helper-Function
  * 
  * Spiteful Spoofer will rotate through scraped user-agent string and proxies
